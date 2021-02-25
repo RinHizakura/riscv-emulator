3dc05dd739dd736bb6d340de0d89fbb85a8aa14c
RinHizakura <s921975628@gmail.com>
2021-02-26 00:13:37 +0800

    Implement plic write

--- src/plic.c
+++ src/plic.c
@@ -46,6 +46,42 @@ bool write_plic(riscv_plic *plic,
                 uint64_t value,
                 riscv_exception *exc)
 {
-    // TODO
-    return false;
+    // support only word-aligned and word size access now
+    if ((size != 32) || ((addr & ~0x3) != addr)) {
+        exc->exception = StoreAMOAccessFault;
+        return false;
+    }
+
+    value &= 0xffffffff;
+
+    if (addr >= PLIC_PRIORITY && addr < PLIC_PRIORITY_END) {
+        plic->priority[(addr - PLIC_PRIORITY) / 4] = value;
+    }
+
+    else if (addr >= PLIC_PENDING && addr < PLIC_PENDING_END) {
+        plic->pending[(addr - PLIC_PENDING) / 4] = value;
+    }
+
+    else if (addr >= PLIC_ENABLE && addr < PLIC_ENABLE_END) {
+        plic->enable[(addr - PLIC_ENABLE) / 4] = value;
+    } else {
+        switch (addr) {
+        case PLIC_THRESHOLD_0:
+            plic->threshold[0] = value;
+            break;
+        case PLIC_CLAIM_0:
+            plic->claim[0] = value;
+            break;
+        case PLIC_THRESHOLD_1:
+            plic->threshold[1] = value;
+            break;
+        case PLIC_CLAIM_1:
+            plic->claim[1] = value;
+            break;
+        default:
+            exc->exception = StoreAMOAccessFault;
+            return false;
+        }
+    }
+    return true;
 }
